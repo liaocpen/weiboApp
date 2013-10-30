c85cb2f61c2db00e775853f2063f8a2a98253ea3
liaocpen <wcpen@qq.com>
2013-10-30 17:57:27 +0800

    no message

---
+++ weiboApp/weiboApp/controller/InitViewController.h
@@ -0,0 +1,16 @@
+//
+//  InitViewController.h
+//  weiboApp
+//
+//  Created by lanhu on 13-10-30.
+//  Copyright (c) 2013年 Liao_Cpen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InitViewController : UIViewController<UIWebViewDelegate>
+
+
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
+@end
---
+++ weiboApp/weiboApp/controller/InitViewController.m
@@ -0,0 +1,38 @@
+//
+//  InitViewController.m
+//  weiboApp
+//
+//  Created by lanhu on 13-10-30.
+//  Copyright (c) 2013年 Liao_Cpen. All rights reserved.
+//
+
+#import "InitViewController.h"
+
+@interface InitViewController ()
+
+@end
+
+@implementation InitViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
