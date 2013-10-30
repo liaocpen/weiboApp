6b272f7578809fb48500d31567032b0597d97b8e
liaocpen <wcpen@qq.com>
2013-10-30 18:38:24 +0800

    no message

--- weiboApp/weiboApp/controller/InitViewController.m
+++ weiboApp/weiboApp/controller/InitViewController.m
@@ -27,12 +27,27 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    NSString *urlString = @"https://api.weibo.com/oauth2/authorize?client_id=2909579077&redirect_uri=http://www.baidu.com&response_type=code&display=mobile&state=authorize";
+    NSURLRequest *request = [[NSURLRequest alloc] initWithURL:[NSURL URLWithString:urlString]];
+    [self.webView setDelegate:self];
+    [_webView loadRequest:request];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType {
+    NSURL *backURL = [request URL];
+    NSString *backURLString = [backURL absoluteString];
+    //判断是否是授权调用时返回的url
+    if ([backURLString hasPrefix:@"http://www.baidu.com/?"]) {
+        NSLog(@"back url string:%@", backURLString);
+        
+    }
+    return YES;
 }
 
 @end
