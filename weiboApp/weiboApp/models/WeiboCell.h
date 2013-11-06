70d53cafcc53665e305d5dfd9933ecd14056b66e
Liao_Cpen <wcpen@qq.com>
2013-11-06 08:46:25 +0800

    no message

---
+++ weiboApp/weiboApp/models/WeiboCell.h
@@ -0,0 +1,17 @@
+//
+//  WeiboCell.h
+//  weiboApp
+//
+//  Created by Liao_Cpen on 13-11-5.
+//  Copyright (c) 2013年 Liao_Cpen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Status.h"
+
+@interface WeiboCell : UITableViewCell
+
+- (void) setupCell: (Status *)status;
+
+
+@end
---
+++ weiboApp/weiboApp/models/WeiboCell.m
@@ -0,0 +1,45 @@
+//
+//  WeiboCell.m
+//  weiboApp
+//
+//  Created by Liao_Cpen on 13-11-5.
+//  Copyright (c) 2013年 Liao_Cpen. All rights reserved.
+//
+
+#import "WeiboCell.h"
+#import <QuartzCore/QuartzCore.h>
+
+
+@implementation WeiboCell
+
+#define FONT_SIZE 14.0f;
+#define CELL_CONTENT_WIDTH 320.0f;
+#define CELL_CONTENT_MARGIN 5.0f;
+
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+
+
+#pragma mark - Custom Mehtod
+- (void)setupCell:(Status *)status{
+    //用block设置头像
+    __block UIImageView *headImageView = [[UIImageView alloc] initWithFrame:CGRectZero];
+    [headImageView setFrame:CGRectMake(CELL_CONTENT_MARGIN, CELL_CONTENT_MARGIN, <#CGFloat width#>, <#CGFloat height#>)]
+    
+}
+@end
