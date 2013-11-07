96bd605c09f5de714a6c4d73bfccfb8fc894aed6
liaocpen <wcpen@qq.com>
2013-11-07 18:40:19 +0800

    no message

--- weiboApp/weiboApp/models/WeiboCell.m
+++ weiboApp/weiboApp/models/WeiboCell.m
@@ -68,6 +68,62 @@
     [commentLabel setFont:[UIFont systemFontOfSize:FONT_SIZE - 1]];
     [[self contentView] addSubview:commentLabel];
     [commentLabel setText:[NSString stringWithFormat:@"评论:%d 转发:%d",status.commentsCount,status.retweetsCount]];
+    commentLabel.adjustsFontSizeToFitWidth = YES;
+    [commentLabel setTextAlignment:NSTextAlignmentRight];
+    [commentLabel setFrame:CGRectMake(220, 13, 100, 28)];
+    
+    //低栏高度设置
+    CGFloat yHeight = 0.0;
+    
+    //设置微博内容
+    UILabel *contentLabel = [[UILabel alloc] initWithFrame:CGRectZero];
+    [contentLabel setLineBreakMode:NSLineBreakByWordWrapping];
+    [contentLabel setNumberOfLines:0];
+    [contentLabel setFont:[UIFont systemFontOfSize:FONT_SIZE]];
+    [[self contentView] addSubview:contentLabel];
+    
+    //动态获取contentLabel高度
+    //ios 7下 方法废弃了
+//    CGSize constraint = CGSizeMake(CELL_CONTENT_WIDTH - (CELL_CONTENT_MARGIN * 2), MAXFLOAT);
+//    CGSize size = [status.text sizeWithAttributes:@{NSFontAttributeName:[UIFont systemFontOfSize:[UIFont systemFontOfSize:FONT_SIZE]}];
+    
+    
+    NSAttributedString *attributedText = [[NSAttributedString alloc] initWithString:status.text attributes:@{NSAttachmentAttributeName: contentLabel.font}];
+    CGRect rect = [attributedText boundingRectWithSize:(CGSize){CELL_CONTENT_WIDTH - CELL_CONTENT_MARGIN * 2, MAXFLOAT} options:NSStringDrawingUsesLineFragmentOrigin context:nil];
+    CGSize size = rect.size;
+    
+    [contentLabel setText:status.text];
+    [contentLabel setFrame:CGRectMake(CELL_CONTENT_MARGIN, headImageView.frame.origin.y + headImageView.frame.size.height + CELL_CONTENT_MARGIN, CELL_CONTENT_WIDTH - CELL_CONTENT_MARGIN * 2, size.height)];
+    yHeight = contentLabel.frame.origin.y + contentLabel.frame.size.height;
+    
+    //转发状态
+    Status *retwitterStatus = status.retweetedStatus;
+    //有转发
+    if (status.hasRetwitter && ![retwitterStatus isEqual:[NSNull null]]) {
+        //转发内容的文本内容
+        NSString *retwitterContentText = [NSString stringWithFormat:@"%@:%@",retwitterStatus.screenName,retwitterStatus.text];
+        UILabel *retwitterContentLabel = [[UILabel alloc] initWithFrame:CGRectZero];
+        [retwitterContentLabel setBackgroundColor:[UIColor grayColor]];
+        [retwitterContentLabel  setLineBreakMode:NSLineBreakByWordWrapping];
+        [retwitterContentLabel setNumberOfLines:0];
+        [retwitterContentLabel  setFont:[UIFont systemFontOfSize:FONT_SIZE]];
+        [[self contentView] addSubview:retwitterContentLabel];
+        NSAttributedString *attributedText = [[NSAttributedString alloc] initWithString:retwitterContentText attributes:@{NSAttachmentAttributeName: retwitterContentLabel.font}];
+        CGRect rect = [attributedText boundingRectWithSize:(CGSize){CELL_CONTENT_WIDTH - CELL_CONTENT_MARGIN * 2, MAXFLOAT} options:NSStringDrawingUsesLineFragmentOrigin context:nil];
+        CGSize size = rect.size;
+        [retwitterContentLabel setText:retwitterContentText];
+        [retwitterContentLabel setFrame:CGRectMake(6, contentLabel.frame.origin.y + contentLabel.frame.size.height, CELL_CONTENT_WIDTH - CELL_CONTENT_MARGIN * 2, size.height)];
+        yHeight += retwitterContentLabel.frame.size.height;
+        
+        
+        //转发的微博有头像
+        if (status.haveRetwitterImage) {
+            //设置转发的微博
+            __block UIImageView *retwitterImageView = [[UIImageView alloc] initWithFrame:CGRectZero];
+            __block UIImage *retwitterImage = [[UIImage alloc] init];
+            
+        }
+    }
     
 }
 
