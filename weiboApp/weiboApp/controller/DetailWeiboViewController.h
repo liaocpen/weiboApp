5d3d67fac9a922f911386154f2f9f407d20a0dbb
Liao_Cpen <wcpen@qq.com>
2013-11-14 23:37:10 +0800

    no message

---
+++ weiboApp/weiboApp/controller/DetailWeiboViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailWeiboViewController.h
+//  weiboApp
+//
+//  Created by Liao_Cpen on 13-11-14.
+//  Copyright (c) 2013年 Liao_Cpen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailWeiboViewController : UITableViewController
+
+@end
