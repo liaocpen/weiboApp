e59452a9727f4b6ee218746539a81d3652b87321
liaocpen <wcpen@qq.com>
2013-11-19 18:25:48 +0800

    Office commit

--- weiboApp/weiboApp/controller/DetailWeiboViewController.m
+++ weiboApp/weiboApp/controller/DetailWeiboViewController.m
@@ -37,6 +37,10 @@
     _hud = [[MBProgressHUD alloc] init];
     _commentArray = [[NSMutableArray alloc] init];
     _page = 1;
+    self.tableView.contentInset = UIEdgeInsetsMake(0, 0, 49, 0);
+    if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 7.0) {
+        self.edgesForExtendedLayout = UIRectEdgeNone;
+    }
     [self continueLoadData:_page];
 }
 
@@ -80,7 +84,8 @@
 
 - (NSString *) getTimeString:(NSString *)string  {
     NSDateFormatter *inputFormatter = [[NSDateFormatter alloc] init];
-    [[inputFormatter setLocale:[[NSLocale alloc] initWithLocaleIdentifier:@"en_US"]];
+    
+    [inputFormatter setLocale:[[NSLocale alloc] initWithLocaleIdentifier:@"en_US"]];
      [inputFormatter setDateFormat:@"EEE MMM dd HH:mm:ss Z yyyy"];
      NSDate *inputDate = [inputFormatter dateFromString:string];
      
@@ -101,7 +106,7 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return [_commentArray count];
+    return 1 + [_commentArray count];
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
@@ -134,6 +139,7 @@
         NSDictionary *dictionary = [_commentArray objectAtIndex:indexPath.row - 1];
         NSString *timeString = [dictionary objectForKey:@"created_at"];
         //评论时间
+        //评论内容
         NSString *commentString = [dictionary objectForKey:@"text"];
         //评论用户
         NSDictionary *user = [dictionary objectForKey:@"user"];
@@ -163,10 +169,100 @@
         
         //评论内容
         UILabel *commentLabel = [[UILabel alloc] initWithFrame:CGRectZero];
+        [commentLabel setLineBreakMode:NSLineBreakByWordWrapping];
+        [commentLabel setNumberOfLines:0];
+        [commentLabel setFont:[UIFont systemFontOfSize:FONT_SIZE]];
+        
+        NSAttributedString *attributeText = [[NSAttributedString alloc] initWithString:commentString attributes:@{NSFontAttributeName: [UIFont systemFontOfSize:FONT_SIZE]}];
+        CGRect rect = [attributeText boundingRectWithSize:(CGSize){CELL_CONTENT_WIDTH - CELL_CONTENT_MARGIN * 2, MAXFLOAT} options:NSStringDrawingUsesLineFragmentOrigin context:nil];
+        CGSize size = rect.size;
+        [commentLabel setText:commentString];
+        [commentLabel setFrame:CGRectMake(CELL_CONTENT_MARGIN, 30 + CELL_CONTENT_MARGIN * 2, (CELL_CONTENT_WIDTH - CELL_CONTENT_MARGIN * 2), size.height)];
+        commentLabel.tag = 102;
+        [[cell viewWithTag:102] removeFromSuperview];
+        [[cell contentView] addSubview:commentLabel];
+        
+        //选择时背景
+        UIView *selectionView = [[UIView alloc] init];
+        selectionView.backgroundColor = [UIColor whiteColor];
+        [cell setSelectedBackgroundView:selectionView];
+        
+        
+        return cell;
         
     }
 }
 
+#pragma mark - UITableViewDelegate
 
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (indexPath.row == 0) {
+        //高度设置
+        CGFloat yHeight = 70.0;
+        //微博内容高度
+        NSAttributedString *attringText = [[NSAttributedString alloc] initWithString:_status.text attributes:@{NSFontAttributeName: [UIFont systemFontOfSize:FONT_SIZE]}];
+        CGRect rect = [attringText boundingRectWithSize:(CGSize){CELL_CONTENT_WIDTH - CELL_CONTENT_MARGIN * 2, MAXFLOAT} options:NSStringDrawingUsesLineFragmentOrigin context:nil];
+        CGSize size = rect.size;
+        yHeight += (size.height + CELL_CONTENT_MARGIN);
+        
+        //转发情况
+        Status *retwitterStatus = _status.retweetedStatus;
+        //有转发
+        if (_status.hasRetwitter && ![retwitterStatus isEqual:[NSNull null]]) {
+            //转发内容的文本内容
+            NSString *retwitterContentText = [NSString stringWithFormat:@"%@:%@", retwitterStatus.screenName, retwitterStatus.text];
+            NSAttributedString *attributerText = [[NSAttributedString alloc] initWithString:retwitterContentText attributes:@{NSFontAttributeName: [UIFont systemFontOfSize:FONT_SIZE]}];
+            CGRect rect = [attributerText boundingRectWithSize:(CGSize){CELL_CONTENT_WIDTH - CELL_CONTENT_MARGIN * 2, MAXFLOAT} options:NSStringDrawingUsesLineFragmentOrigin context:nil];
+            yHeight += (rect.size.height + CELL_CONTENT_MARGIN);
+            
+            //转发有图像
+            if (_status.haveRetwitterImage) {
+                yHeight += (120 + CELL_CONTENT_MARGIN);
+                
+            }
+            
+        }
+        //无转发
+        else{
+            //微博有图像
+            if (_status.hasImage) {
+                yHeight += (120 + CELL_CONTENT_MARGIN);
+            }
+        }
+        yHeight += 20;
+        return yHeight;
+    }
+    else {
+        //获取一个评论
+        NSDictionary *dictionary = [_commentArray objectAtIndex:indexPath.row - 1];
+        
+        NSString *commentString = [dictionary objectForKey:@"text"];
+        NSAttributedString *attriubuedText = [[NSAttributedString alloc] initWithString:commentString attributes:@{NSFontAttributeName: [UIFont systemFontOfSize:FONT_SIZE]}];
+        CGRect rect = [attriubuedText   boundingRectWithSize:(CGSize){CELL_CONTENT_WIDTH - CELL_CONTENT_MARGIN * 2, MAXFLOAT} options:NSStringDrawingUsesLineFragmentOrigin context:nil];
+        return  (30 + 3 * CELL_CONTENT_MARGIN + rect.size.height);
+    }
+}
+
+#pragma mark -UIScrollViewDelegate
+
+- (void)scrollViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate
+{
+    if (self.tableView.contentOffset.y > self.tableView.contentSize.height - self.tableView.frame.size.height) {
+        if (_totalNum <= 50 || _page > (_totalNum / 50 + 1)) {
+            MBProgressHUD *endHud = [[MBProgressHUD alloc] init];
+            endHud.mode = MBProgressHUDModeText;
+            endHud.labelText = @"提示";
+            endHud.detailsLabelText = @"scroll to the end";
+            [self.tableView addSubview:endHud];
+            [endHud show:YES];
+            [endHud hide:YES afterDelay:1];
+            
+        }
+        else if (_totalNum > [_commentArray count]){
+            [self continueLoadData:++_page];
+        }
+    }
+}
 
 @end
